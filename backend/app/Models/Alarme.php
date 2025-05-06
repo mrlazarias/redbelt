ae5496c08cd7602fa176eddb4d3542cda84b8693
Murilo <muriloazarias97@gmail.com>
2025-05-06 10:19:31 -0300

    feat: cria model Alarme com relacionamentos

--- backend/app/Models/Alarme.php
+++ backend/app/Models/Alarme.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Alarme extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'tipo_alarme_id',
+        'criticidade',
+        'status',
+        'ativo',
+        'data_ocorrencia',
+        'tipo',
+        'deleted_by'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tipoAlarme()
+    {
+        return $this->belongsTo(TipoAlarme::class);
+    }
 }
